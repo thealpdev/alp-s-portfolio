2dce0d7f25fa8ca676c8d1859c90361303da9d5c
thealpdev <senin-mailin@example.com>
2026-01-03 23:37:33 +0300

    yapilandirma: static export eklendi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Siteyi statik HTML dosyalarına dönüştürür
+  images: {
+    unoptimized: true, // GitHub Pages resim optimizasyonu yapamadığı için bu gereklidir
+  },
 };
 
 export default nextConfig;
